07a609ab94e3080ffe1b5987357922fd75c50efd
Ratul <rhratul12@gmail.com>
2024-11-30 20:00:19 +0600

    fix cors origin

--- server/config/express.js
+++ server/config/express.js
@@ -17,7 +17,8 @@ export const configureExpress = () => {
   app.use(cors({
     origin:'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials : true
   }));
 
   app.use(helmet({
