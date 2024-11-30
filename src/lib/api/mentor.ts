927b2981f5cc3ae3c2403ff8a04e2682d2d71c2c
Devlab Solution Limited <147203831+devlabsolutionlimited@users.noreply.github.com>
2024-12-01 00:34:41 +0600

    Update src/lib/api/mentor.ts [BoltSync]

---
+++ src/lib/api/mentor.ts
@@ -0,0 +1,42 @@
+import api from '@/lib/http';
+import type { Mentor } from '@/types/mentor';
+
+export const getMentorDashboard = async () => {
+  const response = await api.get('/api/mentors/dashboard');
+  return response.data;
+};
+
+export const getMentorAnalytics = async () => {
+  const response = await api.get('/api/mentors/analytics');
+  return response.data;
+};
+
+export const getMentorProfile = async () => {
+  const response = await api.get('/api/mentor/profile');
+  return response.data;
+};
+
+export const updateBasicInfo = async (data: Partial<Mentor>) => {
+  const response = await api.put('/api/mentor/profile/basic', data);
+  return response.data;
+};
+
+export const updateExpertise = async (data: { expertise: string[]; languages: string[] }) => {
+  const response = await api.put('/api/mentor/profile/expertise', data);
+  return response.data;
+};
+
+export const updateEducation = async (data: { education: Mentor['education'] }) => {
+  const response = await api.put('/api/mentor/profile/education', data);
+  return response.data;
+};
+
+export const updateSocialLinks = async (data: { socialLinks: Mentor['socialLinks'] }) => {
+  const response = await api.put('/api/mentor/profile/social', data);
+  return response.data;
+};
+
+export const updateCustomUrl = async (customUrl: string) => {
+  const response = await api.put('/api/mentor/profile/custom-url', { customUrl });
+  return response.data;
+};
