20530d26ae1a889b6f0dba7ca03557b9088eeece
Devlab Solution Limited <147203831+devlabsolutionlimited@users.noreply.github.com>
2024-12-02 02:30:14 +0600

    Update server/config/express.js [BoltSync]

--- server/config/express.js
+++ server/config/express.js
@@ -18,7 +18,7 @@ export const configureExpress = () => {
 
   // Security middleware
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
