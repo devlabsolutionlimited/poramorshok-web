37bea2db4f6363acb728554d603934ffe2296153
Devlab Solution Limited <147203831+devlabsolutionlimited@users.noreply.github.com>
2024-12-02 15:34:09 +0600

    Update server/routes/student.profile.routes.js [BoltSync]

--- server/routes/student.profile.routes.js
+++ server/routes/student.profile.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { body } from 'express-validator';
 import { protect, authorize } from '../middleware/auth.js';
 import { validate } from '../middleware/validate.js';
-import { upload } from '../config/multer.js';
+import { upload } from '../middleware/upload.js';
 import {
   getProfile,
   updateProfile,
